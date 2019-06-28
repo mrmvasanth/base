c4a6a56844f29920ccc580f1e437e1ed4da17d09
Vasanth Muralikrishnan <42834098+mrmvasanth@users.noreply.github.com>
2019-06-28 17:18:44 +0530

    Basic Options added

---
+++ myapp/src/main/java/com/packs/myapp/constants/AppConstants.java
@@ -0,0 +1,4 @@
+package com.packs.myapp.constants;
+
+public interface AppConstants {
+}
---
+++ myapp/src/main/java/com/packs/myapp/constants/MessageConstants.java
@@ -0,0 +1,6 @@
+package com.packs.myapp.constants;
+
+public interface MessageConstants {
+    String SERVER_ALIVE="Server is alive and running";
+
+}
